d9e9475c4bff08c65ed6ceae9429fee77dd143ef
zhchwolf <zhchwolf@sina.com>
2018-12-04 16:42:11 +0800

    add django admin setup

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+info_list=[]
+
+def userInfor(req):
+
+    if req.method=="POST":
+        username=req.POST.get("username",None)
+        sex=req.POST.get("sex",None)
+        email=req.POST.get("email",None)
+
+        info={"username":username,"sex":sex,"email":email}
+        info_list.append(info)
+
+    return render(req,"userInfor.html",{"info_list":info_list})

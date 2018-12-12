b0f823bf74d9e0956fd8868eb6226c246a06c6db
zhchwolf <zhchwolf@sina.com>
2018-12-12 13:23:15 +0800

    add django sqllite

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
-
+from blog import models
 # Create your views here.
 info_list=[]
 
-def userInfor(req):
+def userInfo(req):
 
     if req.method=="POST":
         username=req.POST.get("username",None)
         sex=req.POST.get("sex",None)
         email=req.POST.get("email",None)
 
-        info={"username":username,"sex":sex,"email":email}
-        info_list.append(info)
+        # info={"username":username,"sex":sex,"email":email}
+        # info_list.append(info)
 
-    return render(req,"userInfor.html",{"info_list":info_list})
+        models.UserInfo.objects.create(
+            username=username,
+            sex=sex,
+            email=email
+        )
+    info_list = models.UserInfo.objects.all()
+    return render(req,"userInfo.html",{"info_list":info_list})

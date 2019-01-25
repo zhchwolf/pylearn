6321800cf855db383059387324dfe16a1e1d3e3a
zhchwolf <zhchwolf@sina.com>
2019-01-25 14:20:05 +0800

    commit pending nkb123

---
+++ data_base/csv/csv_pd01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding:utf-8
+
+import sys
+import os
+import glob
+import csv
+import pandas
+
+self_file = sys.argv[0]
+# input_file = sys.argv[1]
+# output_file = sys.argv[2]
+

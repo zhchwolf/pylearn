f425ce0e3776793c20337cb64be25074dd71b8c6
zhchwolf <zhchwolf@sina.com>
2019-01-28 22:23:35 +0800

    update softmax

--- data_analysis/DeepLearning/activationFunction_01.py
+++ data_analysis/DeepLearning/activationFunction_01.py
@@ -60,4 +60,20 @@ print(y)
 def identify_function(x):
     return x
 
+def softmax(a):
+    exp_a =np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+def softmax_c(a):
+    c = max(a)
+    exp_a =np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+a = np.array([0.3, 2.9, 4.0])
+print(softmax(a))
+print(sum(softmax(a)))
 

2d3fe3f22534ca98c8c0fa3c6ae83d2e05697e05
zhchwolf <zhchwolf@sina.com>
2019-09-03 17:28:35 +0800

    update laptop

--- data_analysis/DeepLearning/activationFunction_01.py
+++ data_analysis/DeepLearning/activationFunction_01.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import matplotlib.pylab as plt
 
-a = np.array([-1.0,1.0,3.0])
-b = a>0
+a = np.array([-1.0, 1.0, 3.0])
+b = a > 0
 print(b)
 print(b.astype(np.int))
+
 
 def step_function(x):
     if x>0:
@@ -13,22 +15,27 @@ def step_function(x):
     else:
         return 0
 
+
 def step_function_01(x):
     y= x>0
     return y.astype(np.int)
 
-import matplotlib.pylab as plt
-def step_function_02(x):
-    return np.array(x>0, dtype=np.int)
 
-x = np.arange(-5.0,5.0,0.1)
+def step_function_02(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+x = np.arange(-5.0, 5.0, 0.1)
 y = step_function_02(x)
-plt.plot(x,y)
+plt.plot(x, y)
 plt.ylim(-0.1,1.1)
 plt.show()
 
+
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
+
+
 x1 = np.array([-1.0,1.0,2.0])
 print(sigmoid(x1))
 
@@ -47,8 +54,10 @@ plt.plot(x,y)
 plt.ylim(-0.1,1.1)
 plt.show()
 
+
 def relu(x):
     return np.maximum(0,x)
+
 
 x3 = np.array([1,2])
 print(x3.shape)
@@ -57,8 +66,10 @@ print(w.shape)
 y = np.dot(x3,w)
 print(y)
 
+
 def identify_function(x):
     return x
+
 
 def softmax(a):
     exp_a =np.exp(a)
@@ -66,12 +77,14 @@ def softmax(a):
     y = exp_a / sum_exp_a
     return y
 
+
 def softmax_c(a):
     c = max(a)
-    exp_a =np.exp(a-c)
+    exp_a = np.exp(a-c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
+
 
 a = np.array([0.3, 2.9, 4.0])
 print(softmax(a))
